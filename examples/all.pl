714a7cf9aba8a3f451b950fa57713e1ceb94757e
spek <spek@localhost>
2015-07-18 11:35:05 -0600

    Engine framework - fixes bug 42 -- correct example files

--- examples/all.pl
+++ examples/all.pl
@@ -6,19 +6,17 @@ use Devel::Examine::Subs;
 
 
 my $params = {
-                file => 't/sample.data', 
-                engine => 'all', 
+                file => '../t/sample.data', 
               };
 
 my $des = Devel::Examine::Subs->new($params);
 
-my $all = $des->run($params);
+my $all = $des->run({engine => 'all'});
 
 print "$_\n" for @$all;
 
 $all = $des->all({
-                file => 't/sample.data', 
-                engine => 'all',
+                file => '../t/sample.data', 
 });
 
 print scalar(@$all) . "\n";
--- examples/has.pl
+++ examples/has.pl
@@ -4,22 +4,20 @@ use strict;
 
 use Devel::Examine::Subs;
 
-my $des = Devel::Examine::Subs->new($params);
-
 my $params = {
-                file => 't/sample.data', 
+                file => '../t/sample.data', 
                 engine => 'has', 
                 search => 'this',
               };
 
+my $des = Devel::Examine::Subs->new($params);
 
 my $has = $des->run($params);
 
 print "$_\n" for @$has;
 
 $has = $des->has({
-                file => 't/sample.data', 
-                engine => 'has', 
+                file => '../t/sample.data', 
                 search => 'this',
 });
 

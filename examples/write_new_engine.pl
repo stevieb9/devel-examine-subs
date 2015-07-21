bed3aff19c4155413e7744ed6f898d4af7c98928
spek <spek@localhost>
2015-07-21 13:20:42 -0600

    - updated examples/write_new_engine.pl

--- examples/write_new_engine.pl
+++ examples/write_new_engine.pl
@@ -12,36 +12,37 @@ my $orig = 't/sample.data.orig';
 my $params = {
                 file => $file,
                 search => 'this',
+#                pre_proc => ,
+#                pre_proc_return => 1,
+#                pre_proc_dump => 1,
                 pre_filter => 'file_lines_contain',
-#                pre_filter_dump => 2,
+#                pre_filter_dump => 1,
 #                pre_filter_return => 1,
-                engine => \&new_has,
+                engine => has(),
+#                engine_return => 1,
+#                engine_dump => 1,
 #                core_dump => 1,
-#                engine_dump => 1,
-#                engine_dump => 1,
 #                copy => 't/inject_after.data',
 #                code => ['# comment line one', '# comment line 2' ],
               };
 
 
-sub new_has {
+sub has {
 
-    my $p = shift;
-    my $struct = shift;
+    return sub {
 
-    print Dumper $struct;
-    my @has;
+        my $p = shift;
+        my $struct = shift;
 
-    my $file = (keys %$struct)[0];
+        my $file = (keys %$struct)[0];
 
-    @has = keys %{$struct->{$file}{subs}};
+        my @has = keys %{$struct->{$file}{subs}};
 
-    return \@has;
+        return \@has;
+    };
 }
 
 my $des = Devel::Examine::Subs->new($params);
 my $struct = $des->run($params);
 
-print "$_\n" for @$struct;
-
-#print Dumper $struct;
+print Dumper $struct;

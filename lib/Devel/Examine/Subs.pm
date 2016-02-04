ce1cb492852d7afc13549d7d2ddffae0c15f8992
Steve Bertrand <steveb@cpan.org>
2016-02-04 07:47:45 -0700

    bumped ver to 1.65

--- lib/Devel/Examine/Subs.pm
+++ lib/Devel/Examine/Subs.pm
@@ -3,7 +3,7 @@ use 5.008;
 use warnings;
 use strict;
 
-our $VERSION = '1.64';
+our $VERSION = '1.65';
 
 use Carp;
 use Data::Compare;
--- lib/Devel/Examine/Subs/Sub.pm
+++ lib/Devel/Examine/Subs/Sub.pm
@@ -3,7 +3,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.64';
+our $VERSION = '1.65';
 
 use Data::Dumper;
 

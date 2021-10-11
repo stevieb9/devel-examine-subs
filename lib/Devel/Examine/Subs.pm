4f2e68bd9a057a3dd22495f317e435c74657bcdf
Steve Bertrand <steveb@cpan.org>
2021-10-11 12:18:44 -0700

    Prep for 1.73

--- lib/Devel/Examine/Subs.pm
+++ lib/Devel/Examine/Subs.pm
@@ -3,7 +3,7 @@ use 5.008;
 use warnings;
 use strict;
 
-our $VERSION = '1.72';
+our $VERSION = '1.73';
 
 use Carp;
 use Data::Compare;
--- lib/Devel/Examine/Subs/Engine.pm
+++ lib/Devel/Examine/Subs/Engine.pm
@@ -3,7 +3,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.72';
+our $VERSION = '1.73';
 
 use Carp;
 use Data::Dumper;
